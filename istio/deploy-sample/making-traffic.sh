ff5f087d5be6d92becb9a2a97773ca048dc8faef
jaeho.p <jaeho.p@hanwha.com>
2025-12-04 23:40:03 +0000

    update code

---
+++ istio/deploy-sample/making-traffic.sh
@@ -0,0 +1,1 @@
+for i in {1..50}; do curl -s http://192.168.2.93:31354/productpage > /dev/null; done
--- istio/istio.sh
+++ istio/istio.sh
@@ -1,3 +1,8 @@
+curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.28.1 sh -
+cd istio-1.28.1
+export PATH=$PWD/bin:$PATH
+
+
 helm repo add istio https://istio-release.storage.googleapis.com/charts
 helm repo update
 helm install istio-base istio/base -n istio-system --create-namespace
@@ -7,7 +12,7 @@ helm install istiod istio/istiod -n istio-system \
   --set pilot.resources.requests.memory=512Mi \
   --set telemetry.enabled=true
 
-#helm install istio-ingress istio/gateway -n istio-system
+helm install istio-ingress istio/gateway -n istio-system
 
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.22/samples/addons/kiali.yaml -n istio-system
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.22/samples/addons/jaeger.yaml -n istio-system
